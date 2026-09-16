8adbe65530dc5a0bc032ce5411718a8d0e525ffb
Gholami-Bahareh <baharehgholamii86@gmail.com>
2026-09-16 23:17:37 +0200

    Friend-card.js

--- tests/friend-cards.spec.js
+++ tests/friend-cards.spec.js
@@ -1,14 +1,13 @@
 const { test, expect } = require('@playwright/test');
 const { login } = require('../api/auth');
+const {FriendCardsApi} = require('../api/friend-cards');
 
 test('Get user friend cards', async ({ request }) => {
     const token = await login();
 
-    const response = await request.get('/api/v2/FriendCards/User/FriendCards', {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
+    const friendCardsApi = new FriendCardsApi(request, token);
+
+    const response = await friendCardsApi.getFriendCards();
 
     expect(response.status()).toBe(200);
 
